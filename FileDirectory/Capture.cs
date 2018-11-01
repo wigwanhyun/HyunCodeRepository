6cd878c0fbfcdf4b7d5b909183dc0710dd17b490
wigwanhyun <44451331+wigwanhyun@users.noreply.github.com>
2018-11-01 14:33:50 +0900

    Capture 유틸 커밋

---
+++ FileDirectory/Capture.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using System.IO;
+
+namespace Capture
+{
+    //참조로 System.Drawing 추가해야됨
+    class Capture
+    {
+        public static Image CaptureScreen()
+        {
+            return CaptureWindow(User32.GetDesktopWindow());
+        }
+
+        /// <summary>
+        /// Creates an Image object containing a screen shot of a specific window
+        /// </summary>
+        /// <param name="handle">The handle to the window. (In windows forms, this is obtained by the Handle property)</param>
+        /// <returns></returns>
+        public static Image CaptureWindow(IntPtr handle)
+        {
+            // get te hDC of the target window
+            IntPtr hdcSrc = User32.GetWindowDC(handle);
+            // get the size
+            User32.RECT windowRect = new User32.RECT();
+            User32.GetWindowRect(handle, ref windowRect);
+            int width = windowRect.right - windowRect.left;
+            int height = windowRect.bottom - windowRect.top;
+            // create a device context we can copy to
+            IntPtr hdcDest = GDI32.CreateCompatibleDC(hdcSrc);
+            // create a bitmap we can copy it to,
+            // using GetDeviceCaps to get the width/height
+            IntPtr hBitmap = GDI32.CreateCompatibleBitmap(hdcSrc, width, height);
+            // select the bitmap object
+            IntPtr hOld = GDI32.SelectObject(hdcDest, hBitmap);
+            // bitblt over
+            GDI32.BitBlt(hdcDest, 0, 0, width, height, hdcSrc, 0, 0, GDI32.SRCCOPY);
+            // restore selection
+            GDI32.SelectObject(hdcDest, hOld);
+            // clean up
+            GDI32.DeleteDC(hdcDest);
+            User32.ReleaseDC(handle, hdcSrc);
+
+            // get a .NET image object for it
+            Image img = Image.FromHbitmap(hBitmap);
+            // free up the Bitmap object
+            GDI32.DeleteObject(hBitmap);
+
+            return img;
+        }
+
+        /// <summary>
+        /// Captures a screen shot of a specific window, and saves it to a file
+        /// </summary>
+        /// <param name="handle"></param>
+        /// <param name="filename"></param>
+        /// <param name="format"></param>
+        public void CaptureWindowToFile(IntPtr handle, string filename, ImageFormat format)
+        {
+            Image img = CaptureWindow(handle);
+            img.Save(filename, format);
+        }
+
+        /// <summary>
+        /// Captures a screen shot of the entire desktop, and saves it to a file
+        /// </summary>
+        /// <param name="filename"></param>
+        /// <param name="format"></param>
+        /// <summary>
+        /// Helper class containing Gdi32 API functions
+        /// </summary>
+        private class GDI32
+        {
+
+            public const int SRCCOPY = 0x00CC0020; // BitBlt dwRop parameter
+
+            [DllImport("gdi32.dll")]
+            public static extern bool BitBlt(IntPtr hObject, int nXDest, int nYDest,
+                int nWidth, int nHeight, IntPtr hObjectSource,
+                int nXSrc, int nYSrc, int dwRop);
+            [DllImport("gdi32.dll")]
+            public static extern IntPtr CreateCompatibleBitmap(IntPtr hDC, int nWidth,
+                int nHeight);
+            [DllImport("gdi32.dll")]
+            public static extern IntPtr CreateCompatibleDC(IntPtr hDC);
+            [DllImport("gdi32.dll")]
+            public static extern bool DeleteDC(IntPtr hDC);
+            [DllImport("gdi32.dll")]
+            public static extern bool DeleteObject(IntPtr hObject);
+            [DllImport("gdi32.dll")]
+            public static extern IntPtr SelectObject(IntPtr hDC, IntPtr hObject);
+        }
+
+        /// <summary>
+        /// Helper class containing User32 API functions
+        /// </summary>
+        private class User32
+        {
+            [StructLayout(LayoutKind.Sequential)]
+            public struct RECT
+            {
+                public int left;
+                public int top;
+                public int right;
+                public int bottom;
+            }
+
+            [DllImport("user32.dll")]
+            public static extern IntPtr GetDesktopWindow();
+            [DllImport("user32.dll")]
+            public static extern IntPtr GetWindowDC(IntPtr hWnd);
+            [DllImport("user32.dll")]
+            public static extern IntPtr ReleaseDC(IntPtr hWnd, IntPtr hDC);
+            [DllImport("user32.dll")]
+            public static extern IntPtr GetWindowRect(IntPtr hWnd, ref RECT rect);
+
+        }
+        public static void CaptureScreenToFile(string savePath, string saveFileName)
+        {
+            DirectoryInfo di = new DirectoryInfo(savePath);
+            ImageFormat fmt = ImageFormat.Png;
+            string now = DateTime.Now.ToString("yyyyMMddmmss");
+            string watermarkText = string.Format("RPA PolicyMan™ \n{0}", saveFileName); //워터마크 텍스트 지정
+
+            if (!di.Exists) { di.Create(); }
+            //saveFileName += "_" + now;
+
+            if (saveFileName.Contains("/"))
+            {
+                saveFileName = saveFileName.Replace("/", "_");
+            }
+
+            string FilePath = string.Format("{0}/{1}.png", savePath, saveFileName);
+
+            Image img = CaptureScreen();
+            Font font = new Font("Arial", 60, FontStyle.Bold, GraphicsUnit.Pixel);
+            Color color = Color.FromArgb(80, 0, 0, 0);
+            Point pt = new Point(img.Width / 2 - 80, img.Height / 2);
+            SolidBrush brush = new SolidBrush(color);
+            Stream output = new MemoryStream();
+
+            Graphics graphics = Graphics.FromImage(img);
+            graphics.DrawString(watermarkText, font, brush, pt);
+            graphics.Dispose();
+
+            img.Save(output, fmt);
+            Image imgFinal = Image.FromStream(output);
+
+            Bitmap bmp = new System.Drawing.Bitmap(img.Width, img.Height, img.PixelFormat);
+
+            Graphics graphics2 = Graphics.FromImage(bmp);
+
+            graphics2.DrawImage(imgFinal, new Point(0, 0));
+
+            bmp.Save(FilePath, fmt);
+
+            imgFinal.Dispose();
+
+            img.Dispose();
+        }
+
+    }
+}
