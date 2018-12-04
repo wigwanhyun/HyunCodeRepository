262afd54e3fd28f45c3d49127515197b0d82a1df
wigwanhyun <44451331+wigwanhyun@users.noreply.github.com>
2018-12-04 17:56:17 +0900

    Winform 추가

---
+++ FileDirectory/Study.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileDirectory
+{
+    class Study
+    {
+        static void Main()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                double meter = i * 0.3048;
+                Console.WriteLine("{0} FT = {1:0.0000} M", i, meter);
+            }
+        }
+    }
+}
---
+++ TestConsole/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintFeetToMeter(1, 20);
+            PrintMeterToFeet(1, 20);
+
+
+            Console.Read();
+        }
+
+        static double FeetToMeter(int feet)
+        {
+            return feet * 0.3048;
+        }
+        static double MeterToFeet(int meter)
+        {
+            return meter / 0.3048;
+        }
+
+        static void PrintFeetToMeter(int start, int stop)
+        {
+            for (int feet = start; feet <= stop; feet++)
+            {
+                double meter = FeetToMeter(feet);
+                Console.WriteLine("{0} FT = {1:0.0000} M", feet, meter);
+            }
+        }
+
+        static void PrintMeterToFeet(int start, int stop)
+        {
+            for (int meter = start; meter <= stop; meter++)
+            {
+                double feet = MeterToFeet(meter);
+                Console.WriteLine("{0} M = {1:0.0000} ft", meter, feet);
+            }
+        }
+    }
+}
