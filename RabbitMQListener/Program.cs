4aa1b37169e319164e9c25140ea21265b5ecaad7
위 관현 <dnlrhksgus0511@gmail.com>
2019-03-07 09:33:15 +0900

    MQListener추가

---
+++ RabbitMQListener/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+namespace RabbitMQListener
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.UserName = "sa";
+            factory.Password = "eloi1!";
+            factory.HostName = "127.0.0.1";
+            factory.Port = 5672;
+
+            IConnection conn = factory.CreateConnection();
+            IModel channel = conn.CreateModel();
+            byte[] messageBodyBytes = System.Text.Encoding.UTF8.GetBytes("Hello, world!");
+            channel.BasicPublish("AMQP default", "33", null, messageBodyBytes);
+        }
+    }
+}
