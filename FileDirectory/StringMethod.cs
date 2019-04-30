343480a19a0dd7c04643d50619fd021d32447b5a
위 관현 <dnlrhksgus0511@gmail.com>
2019-04-30 09:49:09 +0900

    String 으로 Method 호출하는법 추가

---
+++ FileDirectory/StringMethod.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileDirectory
+{
+    class StringMethod
+    {
+        //public object Translate(string fileName)
+        //{
+        //    EloiLogger.Info("Entry");
+        //    EloiLogger.Debug($"Param : {fileName}");
+        //    if (!FileManager.IsFileExists(fileName))
+        //    {
+        //        EloiLogger.Debug($"{fileName} 파일 처리가 이미 되었거나 없는 파일입니다.");
+        //        return null;
+        //    }
+        //    while (FileManager.IsFileLocked(fileName)) ;
+
+        //    string fileContent = FileManager.GetFileContent(fileName);
+        //    string typeName = fileName.Split('-')[0].Substring(fileName.LastIndexOf("/") + 1);
+
+        //    Type type = Assembly.GetExecutingAssembly().GetType($"NxP.Console.FsiDataWatcher.Model.{typeName}");
+
+        //    object targetClass = Activator.CreateInstance(type);
+
+        //    object[] deserializeParam = new string[] { fileContent };
+        //    object fctiJsonModel = type.GetMethod("Deserialize").Invoke(targetClass, deserializeParam);
+
+        //    object[] translateParam = new object[] { fctiJsonModel };
+        //    object fctiDbModel = type.GetMethod("Translate").Invoke(targetClass, translateParam);
+
+        //    EloiLogger.Info("Terminate");
+        //    return fctiDbModel;
+        //}
+    }
+}
